e3f127753bb351a77bcf4c1aab7f3e5b3dc93381
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-09-19 17:06:12 -0500

    Rutas

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,84 +1,24 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-// Asegurarse de que AngularJS está listo antes de ejecutar el código
-angular.module('myApp').run(function ($rootScope) {
-    $rootScope.$on('$routeChangeSuccess', function () {
-        $('#sidebarMenu').removeClass('active');
-        $('#overlay').removeClass('active');
-        $('.footerToogle').removeClass('active');
-    });
-});
+app_opherton.config(['$httpProvider', function ($httpProvider, $rootScope) {
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+}]);
 
-// Configuración de las rutas
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/home', {
-            templateUrl: 'views/home.html',
-            controller: 'HomeController'
-        })
-        .when('/login', {
-            templateUrl: 'views/login.html',
-            controller: 'LoginController'
-        })
-        .when('/register', {
-            templateUrl: 'views/register.html',
-            controller: 'RegisterController'
-        })
-        .when('/reset', {
-            templateUrl: 'views/reset.html',
-            controller: 'ResetController'
-        })
-        .when('/categories', {
-            templateUrl: 'views/categories.html',
-            controller: 'CategoriesController'
-        })
-        .when('/oficial-stores', {
-            templateUrl: 'views/oficial-stores.html',
-            controller: 'OficialStoresController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
-
-// Controladores
-app.controller('HomeController', function ($scope) {
-    // Lógica para el controlador de la página principal
-});
-
-app.controller('CategoriesController', function ($scope) {
-    // Lógica para el controlador de Categorías
-});
-
-app.controller('LoginController', function ($scope) {
-    // Lógica para el controlador de Login
-});
-
-app.controller('RegisterController', function ($scope) {
-    // Lógica para el controlador de Register
-});
-
-app.controller('ResetController', function ($scope) {
-    // Lógica para el controlador de Reset
-});
-
-app.controller('OficialStoresController', function ($scope) {
-    // Lógica para el controlador de Reset
-});
-
-// Manejo de la visibilidad del header y footer
-app.run(function ($rootScope, $location) {
-    $rootScope.isHeaderVisible = true;
-    $rootScope.isFooterVisible = true;
-
-    $rootScope.$on('$routeChangeSuccess', function () {
-        var path = $location.path();
-        if (path === '/login' || path === '/register' || path === '/reset') {
-            $rootScope.isHeaderVisible = false;
-            $rootScope.isFooterVisible = false;
-        } else {
-            $rootScope.isHeaderVisible = true;
-            $rootScope.isFooterVisible = true;
-        }
-    });
-});
+app_opherton.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', { templateUrl: 'views/home.html', reloadOnSearch: false });
+    $routeProvider.when('/home', { templateUrl: 'views/home.html', reloadOnSearch: false });
+    $routeProvider.when('/oficial-stores', { templateUrl: 'views/oficial-stores.html', reloadOnSearch: false });
+    $routeProvider.when('/categories', { templateUrl: 'views/categories.html', reloadOnSearch: false });
+    $routeProvider.when('/marketplace', { templateUrl: 'views/marketplace.html', reloadOnSearch: false });
+    $routeProvider.when('/register', { templateUrl: 'views/register.html', reloadOnSearch: false });
+    $routeProvider.when('/register/:id', { templateUrl: 'views/register.html', reloadOnSearch: false });
+    $routeProvider.when('/login', { templateUrl: 'views/login.html', reloadOnSearch: false });
+    $routeProvider.when('/login/:id', { templateUrl: 'views/login.html', reloadOnSearch: false });
+    $routeProvider.when('/reset', { templateUrl: 'views/reset.html', reloadOnSearch: false });
+    $routeProvider.when('/profile/:id', { templateUrl: 'views/profile.html', reloadOnSearch: false });
+    $routeProvider.when('/profile/', { templateUrl: 'views/profile.html', reloadOnSearch: false });
+    $routeProvider.when('/profile_img/', { templateUrl: 'views/profile_img.html', reloadOnSearch: false });
+    $routeProvider.when('/no-conexion', { templateUrl: 'views/no-conexion.html', reloadOnSearch: false });
+    $routeProvider.when('/rubros-list/:id/:pais', { templateUrl: 'views/rubros-list.html', reloadOnSearch: false });
+    $routeProvider.when('/store/:id', { templateUrl: 'views/store.html', reloadOnSearch: false });
+}]);
